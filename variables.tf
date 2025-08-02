430d96e4fc30cd6011b2c238214628516445bac2
rafeekshaik <rafeekshaik365@gmail.com>
2025-08-02 18:52:14 +0530

    sg

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  
+}
+variable "environment" {
+  
+}
+variable "common_tags" {
+ 
+}
+variable "sg_name" {
+  
+}
+
+
+variable "description" {
+  
+}
+variable "sg_tags" {
+  default = {}
+}
+variable "vpc_id" {
+  
+}
